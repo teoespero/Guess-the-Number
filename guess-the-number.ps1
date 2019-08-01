1566a17dd49e8969f12a3a66e4e3633ede004590
Teo Espero <38012835+teoespero@users.noreply.github.com>
2019-08-01 11:50:45 -0700

    Code changes
    - Allow the user to specify the range
    - Validate if:
                 - guess > max range
                 - guess = 0

--- guess-the-number.ps1
+++ guess-the-number.ps1
@@ -20,6 +20,7 @@ $playGame = "Yes"                        # Controls when to quit the game
 $status = "Play"                         # Controls the current round of play
 $guess = 0                               # Stores the player's guess
 $reply = ""                              # Stores the player's response when asked to play again
+$range = 0                               # Allow the user to put in a range
 
 ## Display the game's opening screen
 
@@ -33,101 +34,135 @@ Write-Host "`n`n`n`n`n`n`n`n`n`n Press Enter to continue."
 ## Pause the game until the player presses the Enter key
 Read-Host
 
+## get the guessing range
+
+Clear-Host
+
+#################################################################
 ## Loop until the player decides to quit the game
+
 while ($playGame -ne "No") {
+    while($range -eq 0){
+        Clear-Host
 
-    ## Generate the game's random number (between 1 - 100)
+        $range = Read-Host " Enter the max number"
+
+        ## Generate the game's random number between 1 and 
+        ## the max number specified by the user)
     
-    $number = $randomNo.Next(1,101)
+        $number = $randomNo.Next(1,$range)
 
-    ##Clear the Windows command console screen
+        ##Clear the Windows command console screen
 
-    Clear-Host
+        Clear-Host
     
-    ## Loop until the player guesses the secret number
-    while ($status -ne "Stop") {
+        ## Loop until the player guesses the secret number
+        Write-Host $number
+        while ($status -ne "Stop") {
 
-        ## Prompt the player to guess a number
-        while ($guess -eq "") {
-            Clear-Host #Clear the Windows command console screen
-            Write-Host
+            ## Prompt the player to guess a number
+            while ($guess -eq "") {
+                Clear-Host #Clear the Windows command console screen
+                Write-Host
             
-            ## Collect the player's guess
-            $guess = Read-Host " Enter a number between 1 and 100"
-        }
-
-        ## Keep track of the number of guesses made so far
-
-        $noOfGuesses++
-        if ($guess -lt $number) { ## The player's guess was too low            Clear-Host # Clear the Windows command console screen
-        
-            Write-Host "`n Sorry. Your guess was too low. Press Enter to guess again."
-            $guess = "" # Reset the player's guess
-        
-            Read-Host # Pause the game until the player presses the Enter key
-        }
-        elseif ($guess -gt $number) { # The player's guess was too high
-            Clear-Host # Clear the Windows command console screen
-        
-            Write-Host "`n Sorry. Your guess was too high. Press Enter to guess again."
-            $guess = "" # Reset the player's guess
-        
-            Read-Host # Pause the game until the player presses the Enter key
-        }
-        else { # The player has guessed the game's secret number
-            Clear-Host # Clear the Windows command console screen
-        
-            Write-Host "`n Congratulations. You guessed my number! Press Enter to continue."
-        
-            $status = "Stop" # Reset the player's guess
-        
-            Read-Host # Pause the game until the player presses the Enter key
-        }
-    }
-
-    ## Clear the Windows command console screen
-    Clear-Host
-    
-    ## Display the game's opening screen
-    Write-Host "`n                     Game Statistics"
-    Write-Host " -----------------------------------------------------------"
-    Write-Host "`n            The secret number was: $number."
-    Write-Host "`n        You guessed it in $noOfGuesses guesses.`n"    Write-Host " -----------------------------------------------------------"
-    Write-Host "`n`n`n`n`n`n`n`n`n`n`n`n`n`n Press Enter to continue."
-    
-    ## Pause the game until the player presses the Enter key
-    Read-Host
-    
-    ## Clear the Windows command console screen
-    Clear-Host
-    
-    $reply = "" # Stores the player's response when asked to play again
-
-    ## Prompt the player to play another round
-    while ($reply -eq "") {
-        Clear-Host # Clear the Windows command console screen
-        Write-Host
-        
-        ## Collect the player’s answer
-        $reply = Read-Host " Would you like to play again? (Y/N) "
-
-        ## Validate player input, allowing only Y and N as acceptable responses
-        if (($reply -ne "Y") -and ($reply -ne "N")) {
-            $reply = "" # Reset the variable to its default value
+                ## Collect the player's guess
+                $guess = Read-Host " Enter a number between 1 and $range"
             }
-    }
 
-    #The player has elected to play again
-    if ($reply -eq "Y") {
+            ## Keep track of the number of guesses made so far
+
+            $noOfGuesses++
+            if ($guess -gt $range) {                 ## The player's guess was beyond the range                Clear-Host # Clear the Windows command console screen
+        
+                Write-Host "`n Sorry. Your guess was way beyond the range. Press Enter to guess again."
+                $guess = "" # Reset the player's guess
+        
+                Read-Host # Pause the game until the player presses the Enter key
+            }            elseif($guess -eq 0){                ## The player's guess was 0                Clear-Host # Clear the Windows command console screen
+        
+                Write-Host "`n Sorry. Your guess was equal to 0. Press Enter to guess again."
+                $guess = "" # Reset the player's guess
+        
+                Read-Host # Pause the game until the player presses the Enter key            }            else{                ## The player's guess was too low                if ($guess -lt $number) {                     Clear-Host # Clear the Windows command console screen
+        
+                    Write-Host "`n Sorry. Your guess was too low. Press Enter to guess again."
+                    $guess = "" # Reset the player's guess
+        
+                    Read-Host # Pause the game until the player presses the Enter key
+                }
+                elseif ($guess -gt $number) { 
+                    ## The player's guess was too high
+                    
+                    Clear-Host # Clear the Windows command console screen
+        
+                    Write-Host "`n Sorry. Your guess was too high. Press Enter to guess again."
+                    $guess = "" # Reset the player's guess
+        
+                    Read-Host # Pause the game until the player presses the Enter key
+                }
+                else { 
+                    ## The player has guessed the game's secret number
+                    
+                    Clear-Host # Clear the Windows command console screen
+        
+                    Write-Host "`n Congratulations. You guessed my number! Press Enter to continue."
+        
+                    $status = "Stop" # Reset the player's guess
+        
+                    Read-Host # Pause the game until the player presses the Enter key
+                }
+            }
+        }
+
+        ## Clear the Windows command console screen
+        Clear-Host
     
-        ## Reset variables to their default values
-        $number = 0
-        $noOfGuesses = 0
-        $status = "Play"        $guess = 0
-    }
-    else { # The player has decided to quit playing
-        $playGame = "No" # Modify variable indicating that it is time to
-        ## terminate gameplay
+        ## Display the game's opening screen
+        Write-Host "`n                     Game Statistics"
+        Write-Host " -----------------------------------------------------------"
+        Write-Host "`n            The secret number was: $number."
+        Write-Host "`n        You guessed it in $noOfGuesses guesses.`n"        Write-Host " -----------------------------------------------------------"
+        Write-Host "`n`n`n`n`n`n`n`n`n`n`n`n`n`n Press Enter to continue."
+    
+        ## Pause the game until the player presses the Enter key
+        Read-Host
+    
+        ## Clear the Windows command console screen
+        Clear-Host
+    
+        $reply = "" # Stores the player's response when asked to play again
+
+        ## Prompt the player to play another round
+        while ($reply -eq "") {
+            Clear-Host # Clear the Windows command console screen
+            Write-Host
+        
+            ## Collect the player’s answer
+            $reply = Read-Host " Would you like to play again? (Y/N) "
+
+            ## Validate player input, allowing only Y and N as acceptable responses
+            if (($reply -ne "Y") -and ($reply -ne "N")) {
+                $reply = "" # Reset the variable to its default value
+                }
+        }
+
+        
+        if ($reply -eq "Y") {
+            ## The player has elected to play again
+    
+            ## Reset variables to their default values
+
+            $number = 0
+            $noOfGuesses = 0
+            $status = "Play"            $guess = 0
+            $range = 0
+        }
+        else { 
+            ## The player has decided to quit playing
+            
+            $playGame = "No" # Modify variable indicating that it is time to
+            ## terminate gameplay
+        }
     }
 }
 
